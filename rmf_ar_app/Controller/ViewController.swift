e7d0aada5fbe419a588afe96d9286a116e6da8cf
Matthew Booker <mattbooker97@gmail.com>
2021-07-28 17:19:37 +0800

    Added indicator for tracking state
    Signed-off-by: Matthew Booker <mattbooker97@gmail.com>

--- rmf_ar_app/Controller/ViewController.swift
+++ rmf_ar_app/Controller/ViewController.swift
@@ -9,10 +9,11 @@ import UIKit
 import ARKit
 import RealityKit
 
-class ViewController: UIViewController, ARSessionDelegate {
+class ViewController: UIViewController {
         
     @IBOutlet var arView: ARView!
-
+    @IBOutlet weak var trackingStateIndicator: UIButton!
+    
     var networkManager: NetworkManager!
     var robotStateManager: RobotStateManager!
     var buildingMapManager: BuildingMapManager!
@@ -23,6 +24,12 @@ class ViewController: UIViewController, ARSessionDelegate {
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        
+        
+        // Setup the tracking state indicator
+        trackingStateIndicator.layer.cornerRadius = 10
+        trackingStateIndicator.layer.cornerCurve = .continuous
+        trackingStateIndicator.layer.backgroundColor = UIColor.red.cgColor
         
         // Setup all the managers and controllers
         networkManager = NetworkManager()
@@ -38,7 +45,7 @@ class ViewController: UIViewController, ARSessionDelegate {
         arView.session.delegate = self
         
         // Debugging options
-        //        arView.debugOptions = ARView.DebugOptions([.showStatistics, .showWorldOrigin, .showAnchorGeometry, .showAnchorOrigins])
+        arView.debugOptions = ARView.DebugOptions([.showStatistics, .showWorldOrigin])
         
         // Setup AR configuration
         let configuration = ARWorldTrackingConfiguration()
@@ -50,12 +57,21 @@ class ViewController: UIViewController, ARSessionDelegate {
         
         configuration.detectionImages = referenceImages
         configuration.maximumNumberOfTrackedImages = 1
+    
         
         configuration.environmentTexturing = .automatic
         
         arView.session.run(configuration, options: [])        
     }
     
+    @IBAction func taskMenuButtonTapped(_ sender: Any) {
+        taskViewController.modalPresentationStyle = .custom
+        taskViewController.transitioningDelegate = self
+        self.present(taskViewController, animated: true, completion: nil)
+    }
+}
+
+extension ViewController: ARSessionDelegate {
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         
         for arAnchor in anchors {
@@ -65,10 +81,26 @@ class ViewController: UIViewController, ARSessionDelegate {
         }
     }
     
-    @IBAction func taskMenuButtonTapped(_ sender: Any) {
-        taskViewController.modalPresentationStyle = .custom
-        taskViewController.transitioningDelegate = self
-        self.present(taskViewController, animated: true, completion: nil)
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+
+        for arAnchor in anchors {
+
+            // Pass the anchor to the robot state manager
+            robotStateManager.handleARAnchor(anchor: arAnchor)
+        }
+    }
+}
+
+extension ViewController: ARSessionObserver {
+    func session(_ session: ARSession, cameraDidChangeTrackingState camera: ARCamera) {
+        switch camera.trackingState {
+        case .normal:
+            trackingStateIndicator.layer.backgroundColor = UIColor.green.cgColor
+        case .notAvailable:
+            trackingStateIndicator.layer.backgroundColor = UIColor.green.cgColor
+        default:
+            trackingStateIndicator.layer.backgroundColor = UIColor.yellow.cgColor
+        }
     }
 }
 
