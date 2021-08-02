154d9ef826317cce3aa86c8bee67e209cfd7ce0a
Matthew Booker <mattbooker97@gmail.com>
2021-08-02 14:43:57 +0800

    Added relocalization
    Signed-off-by: Matthew Booker <mattbooker97@gmail.com>

--- rmf_ar_app/Model/Localization/RobotTagLocalizer.swift
+++ rmf_ar_app/Model/Localization/RobotTagLocalizer.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import os
 import Combine
 import ARKit
 import RealityKit
@@ -14,81 +15,153 @@ class RobotTagLocalizer {
     
     var arView: ARView
     
+    private var lastUpdateTime: Date
+    
     private var isLocalized = false
+    private var unalignedCounter = 0
+    
+    let logger = Logger(subsystem: Bundle.main.bundleIdentifier!, category: "TrajectoryManager")
     
     init(arView: ARView) {
         self.arView = arView
         
-        NotificationCenter.default.addObserver(self, selector: #selector(runLocalization), name: Notification.Name("robotStatesUpdated"), object: nil)
+        lastUpdateTime = Date.distantPast
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(run), name: Notification.Name("robotStatesUpdated"), object: nil)
     }
     
-    // Sets the ARView world origin to be the world origin as used within the RMF system
-    @objc func runLocalization(_ notification: Notification) -> Void {
+    @objc func run(_ notification: Notification) {
         
-        // Only run if we have not localized
-        if isLocalized {
+        // Check to make sure we only localize at UPDATE_RATE or slower
+        let currentTime = Date()
+        
+        if currentTime.timeIntervalSince(lastUpdateTime) < (1 / ARConstants.Localization.UPDATE_RATE) {
+            logger.debug("Skipping received robot states. Received message too soon after previous")
+            return
+        } else {
+            lastUpdateTime = currentTime
+        }
+        
+        guard let robotsDict = notification.userInfo as? [String: TrackedRobot] else {
+            logger.error("Notification \(notification.name.rawValue)'s user info did not match expected value")
             return
         }
         
         // Only localize if tracking state is good
         switch arView.session.currentFrame?.camera.trackingState {
         case .normal:
-            print("Tracking state normal")
+            // Only consider robots that are tracked and not moving
+            let trackedRobots = robotsDict.filter { $0.value.isTracked && !$0.value.robotState.mode.contains("Moving") }
+            
+            // Check if we are currently tracking any robots
+            if trackedRobots.count == 0 {
+                logger.info("No tracked robots")
+                return
+            }
+            
+            // Sort robots according to most recently seen (i.e. last seen should be as big as possible)
+            let sortedRobots = trackedRobots.values.sorted(by: {first, second in
+                return first.lastSeen > second.lastSeen
+            })
+            
+            // Retrieve the most recently seen robot's state and its corresponding anchor
+            let robot = sortedRobots.first!.robotState
+            guard let anchor = arView.session.currentFrame?.anchors.first(where: {$0.name == robot.robotName}) else {
+                logger.error("Could not retrieve anchor associated with robot: \(robot.robotName)")
+                return
+            }
+            
+            // Check whether to fully localize or just relocalize (steps are slightly different)
+            if !isLocalized {
+                runLocalization(robot: robot, anchor: anchor)
+                isLocalized = true
+            } else {
+                runRelocalization(robot: robot, anchor: anchor)
+            }
+            
         default:
-            print("Tracking state: \((arView.session.currentFrame?.camera.trackingState)!) - Aborting localization")
+            logger.info("Tracking state not good enough for localization - Aborting")
             return
         }
-        
-        guard let trackedRobotsDict = notification.userInfo as? [String: TrackedRobot] else {
-            print("ERROR: Notification \(notification.name)'s user info did not match expected value")
-            return
-        }
-        
-        // Sort robots according to most recently seen (i.e. last seen should be as big as possible)
-        let sortedRobots = trackedRobotsDict.values.sorted(by: {first, second in
-            return first.lastSeen ?? 0 > second.lastSeen ?? 0
-        })
-        
-        if !(sortedRobots.first?.isTracked ?? false) {
-            print("INFO: No tracked robots")
-            return
-        }
-        
-        // Retrieve the robot state and its corresponding anchor
-        guard let robot = sortedRobots.first?.robotState else { return }
-        guard let anchor = arView.session.currentFrame?.anchors.first(where: {$0.name == robot.robotName}) else {return}
-        
-        
-        // Get vector pointing into marker and remove any vertical motion from it
-        var yVec = -simd_float3(anchor.transform[1,0], 0, anchor.transform[1,2])
-        yVec[1] = 0
-
-        let rotationToAlignXYPlane = pointXAxisAt(at: yVec)
-        let rotationToAlignXAxis = Transform(pitch: 0, yaw: Float(robot.locationYaw), roll: 0).matrix.transpose
-        let finalRotation = rotationToAlignXAxis * rotationToAlignXYPlane
-        
-        let rotatedAnchor = finalRotation.transpose * anchor.transform[3]
-        let dx = rotatedAnchor[0] - Float(robot.locationX)
-        let dy = rotatedAnchor[1] - Float(robot.locationY)
-        let dz = rotatedAnchor[2] - 1
-
-        // Need to apply the rotation to the translation vector
-        let translation = finalRotation * simd_float4([dx, dy, dz, 1])
-
-        var finalTransform = finalRotation
-        finalTransform[3] = translation
-        
-        arView.session.setWorldOrigin(relativeTransform: finalTransform)
-        
-        if !isLocalized {
-            // Send a notification that the world origin was updated
-            let localizationData = ["levelName": robot.levelName]
-            NotificationCenter.default.post(name: Notification.Name("setWorldOrigin"), object: nil, userInfo: localizationData)
-        }
-        
-        isLocalized = true
     }
     
+    // Sets the ARView world origin to be the world origin as used within the RMF system
+    func runLocalization(robot: RobotState, anchor: ARAnchor) {
+        logger.info("Beginning localization")
+        
+        // Get vector pointing into marker and remove any vertical motion from it
+        let yVec = -simd_float3(anchor.transform[1,0], 0, anchor.transform[1,2])
+
+        // Calculate the rotations required to align the ARKit frame and RMF frame
+        let rotationToAlignXYPlane = pointXAxisAt(at: yVec)
+        let rotationToAlignXAxis = Transform(pitch: 0, yaw: Float(robot.locationYaw), roll: 0).matrix.transpose
+        let alignmentRotation = rotationToAlignXAxis * rotationToAlignXYPlane
+        
+        // Rotate the anchor to align its frame and then calculate the required translation so
+        // that it is in the same position as the robot
+        let rotatedAnchor = alignmentRotation.transpose * anchor.transform[3]
+        let dx = rotatedAnchor[0] - Float(robot.locationX)
+        let dy = rotatedAnchor[1] - Float(robot.locationY)
+        let dz = rotatedAnchor[2] - ARConstants.Localization.MARKER_HEIGHT
+
+        // Need to apply the rotation to the translation vector
+        let alignmentTranslation = alignmentRotation * simd_float4([dx, dy, dz, 1])
+
+        var alignmentTransform = alignmentRotation
+        alignmentTransform[3] = alignmentTranslation
+        
+        arView.session.setWorldOrigin(relativeTransform: alignmentTransform)
+        
+        // Send a notification that the world origin was updated
+        let localizationData = ["levelName": robot.levelName]
+        NotificationCenter.default.post(name: Notification.Name("setWorldOrigin"), object: nil, userInfo: localizationData)
+    }
+    
+    func runRelocalization(robot: RobotState, anchor: ARAnchor) {
+
+        // Check that the robot state and anchor agree with positioning
+        let dx = anchor.transform[3].x - Float(robot.locationX)
+        let dy = anchor.transform[3].y - Float(robot.locationY)
+        let positionError = sqrt(dx * dx + dy * dy)
+        
+        // The anchors y-axis points out of the marker so we invert it and find the angle it creates to get the yaw
+        let anchorYaw = atan2(-anchor.transform[1,1], -anchor.transform[1,0])
+        
+        let rotationError = anchorYaw - Float(robot.locationYaw)
+        
+        logger.debug("Relocalization: Position error = \(positionError) | Rotation error = \(rotationError)")
+        
+        if abs(rotationError) > (ARConstants.Localization.ANGULAR_THRESHOLD * Float.pi / 180) || positionError > ARConstants.Localization.DISTANCE_THRESHOLD {
+            logger.debug("Positional/Rotation errors over threshold. Count: \(self.unalignedCounter) out of \(ARConstants.Localization.RELOCALIZATION_THRESHOLD)")
+            unalignedCounter += 1
+        } else {
+            unalignedCounter = 0
+            return
+        }
+        
+        if unalignedCounter >= ARConstants.Localization.RELOCALIZATION_THRESHOLD {
+            logger.info("Beginning relocalization")
+            
+            let alignmentRotation = Transform(pitch: 0, yaw: 0, roll: Float(rotationError)).matrix
+            
+            let rotatedAnchor = alignmentRotation.transpose * anchor.transform[3]
+            let dx = rotatedAnchor[0] - Float(robot.locationX)
+            let dy = rotatedAnchor[1] - Float(robot.locationY)
+            let dz = rotatedAnchor[2] - ARConstants.Localization.MARKER_HEIGHT
+            
+            let alignmentTranslation = alignmentRotation * simd_float4([dx, dy, dz, 1])
+
+            var alignmentTransform = alignmentRotation
+            alignmentTransform[3] = alignmentTranslation
+            
+            var transformation = alignmentRotation
+            transformation[3] = alignmentTranslation
+            
+            arView.session.setWorldOrigin(relativeTransform: transformation)
+            unalignedCounter = 0
+            return
+        }
+    }
     
     private func pointXAxisAt(at: SIMD3<Float>) -> float4x4 {
         
