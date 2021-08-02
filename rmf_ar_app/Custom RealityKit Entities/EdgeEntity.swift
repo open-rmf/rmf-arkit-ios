536363284a36ad094b26a0561580d5d61b8a8170
Matthew Booker <mattbooker97@gmail.com>
2021-08-02 14:40:34 +0800

    Improved performance by sharing resources
    Signed-off-by: Matthew Booker <mattbooker97@gmail.com>

--- rmf_ar_app/Custom RealityKit Entities/EdgeEntity.swift
+++ rmf_ar_app/Custom RealityKit Entities/EdgeEntity.swift
@@ -11,12 +11,14 @@ import ARKit
 
 class EdgeEntity: Entity, HasModel {
     
+    static let EDGE_MATERIAL = UnlitMaterial(color: .cyan.withAlphaComponent(0.6))
+    
     // Default constructor is required
     required init() {
         super.init()
     }
     
-    required init(vertex1: Vertex, vertex2: Vertex, color: UIColor) {
+    required init(vertex1: Vertex, vertex2: Vertex) {
         super.init()
         
         let directionVec = simd_float2(vertex2.x, vertex2.y) - simd_float2(vertex1.x, vertex1.y)
@@ -26,7 +28,7 @@ class EdgeEntity: Entity, HasModel {
         let zAxisRotation = atan2(directionVec.y, directionVec.x)
         let edgeRotation = simd_quatf(angle: zAxisRotation, axis: [0,0,1])
         
-        self.components[ModelComponent] = ModelComponent(mesh: .generatePlane(width: edgeLength, height: ARConstants.NavGraph.EDGE_WIDTH), materials: [UnlitMaterial(color: color)])
+        self.components[ModelComponent] = ModelComponent(mesh: .generatePlane(width: edgeLength, height: ARConstants.NavGraph.EDGE_WIDTH), materials: [EdgeEntity.EDGE_MATERIAL])
         
         self.components[Transform] = Transform(scale: [1,1,1], rotation: edgeRotation, translation: [midpoint.x, midpoint.y, ARConstants.NavGraph.Z_OFFSET])
     }
