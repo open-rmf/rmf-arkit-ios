fdd1bd2bbdb7c1e26a9611fb655a4bfdb9b59f7a
Matthew Booker <mattbooker97@gmail.com>
2021-08-05 11:21:47 +0800

    Changed how robot states data is passed
    Signed-off-by: Matthew Booker <mattbooker97@gmail.com>

---
+++ rmf_ar_app/Controller/SettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsViewController.swift
+//  rmf_ar_app
+//
+//  Created by Matthew Booker on 3/8/21.
+//
+
+import Foundation
---
+++ rmf_ar_app/Model/Settings/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsManager.swift
+//  rmf_ar_app
+//
+//  Created by Matthew Booker on 3/8/21.
+//
+
+import Foundation
+
+class SettingsManager {
+    
+    
+}
